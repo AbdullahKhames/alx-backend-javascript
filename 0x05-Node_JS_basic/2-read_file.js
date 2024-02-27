3e85dfcb24229ea7ae21e62b3a32bb6effd6744c
AbdullahKhames <abdullahkhames96@gmail.com>
2024-02-27 20:38:49 +0200

    fixed issue

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const fs = require("node:fs");
+const fs = require('node:fs');
 
-const Course = function (name, students) {
+const Course = function courseConstructor(name, students) {
   this.name = name;
   this.students = students;
 };
@@ -9,49 +9,44 @@ const courses = [];
 
 function countStudents(path) {
   try {
-    let array = null;
     let numberOfStudents = 0;
-    const data = fs.readFileSync(path, "utf-8");
-    const lines = data.split("\r\n");
-    for (const line of lines) {
-      array = line.split(",");
-      if (array[0] == "" || array[0] == "firstname") {
-        continue;
-      }
-      numberOfStudents++;
-      const courseNames = courses.map((course) => course.name);
-      if (courseNames.includes(array[3])) {
-        courses.forEach((course) => {
-          if (course.name == array[3]) {
-            course.students.push(array[0]);
-          }
-        });
-      } else {
-        const newCourse = new Course(array[3], [array[0]]);
-        courses.push(newCourse);
+    const data = fs.readFileSync(path, 'utf-8');
+    const lines = data.split('\r\n');
+    for (let idx = 1; idx < lines.length; idx += 1) {
+      const line = lines[idx].split(',');
+      if (line[0] && line[0] !== 'firstname') {
+        numberOfStudents += 1;
+        const courseNames = courses.map((course) => course.name);
+        if (courseNames.includes(line[3])) {
+          courses.forEach((course) => {
+            if (course.name === line[3]) {
+              course.students.push(line[0]);
+            }
+          });
+        } else {
+          const newCourse = new Course(line[3], [line[0]]);
+          courses.push(newCourse);
+        }
       }
     }
     console.log(`Number of students: ${numberOfStudents}`);
-    for (let index = 0; index < courses.length; index++) {
+    for (let index = 0; index < courses.length; index += 1) {
       const course = courses[index];
       process.stdout.write(
-        `Number of students in ${course.name}: ${
-          course.students.length
-        }. List: `
+        `Number of students in ${course.name}: ${course.students.length}. List: `,
       );
-      for (let j = 0; j < course.students.length; j++) {
+      for (let j = 0; j < course.students.length; j += 1) {
         const student = course.students[j];
         process.stdout.write(student);
-        if (j != course.students.length - 1) {
+        if (j !== course.students.length - 1) {
           process.stdout.write(', ');
         }
-        
       }
       console.log();
     }
   } catch (err) {
     console.error(err);
-    throw new Error("Cannot load the database");
+    throw new Error('Cannot load the database');
   }
 }
 
