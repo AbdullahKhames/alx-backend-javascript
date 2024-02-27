3db12b1d53acd47fb85d008a743961f935ea6e47
AbdullahKhames <abdullahkhames96@gmail.com>
2024-02-27 21:21:04 +0200

    readFileSync

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -8,7 +8,6 @@ if (process.stdin.isTTY) {
   });
   rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
     console.log(`Your name is: ${name}`);
-    console.log('This important software is now closing');
     rl.close();
   });
 } else {
