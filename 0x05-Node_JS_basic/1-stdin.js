cf718a1d4a4618cdf4f209b847f5b865ddb20253
AbdullahKhames <abdullahkhames96@gmail.com>
2024-02-27 17:35:23 +0200

    stdin demo

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,16 @@
 #!/usr/bin/node
-const prompt = require("prompt-sync")({ sigint: true });
+const readline = require("readline-sync");
 
 if (process.stdin.isTTY) {
-  const name = prompt("Welcome to Holberton School, what is your name?\n");
+  const name = readline.question(
+    "Welcome to Holberton School, what is your name?\n"
+  );
   console.log(`Your name is: ${name}`);
   console.log("This important software is now closing");
 } else {
-  process.stdin.on("data", (data) => {
+  process.stdin.on("data", (name) => {
     console.log("Welcome to Holberton School, what is your name?");
-    console.log(`Your name is: ${data.toString()}`);
+    console.log(`Your name is: ${name}`);
     console.log("This important software is now closing");
     process.exit();
   });
