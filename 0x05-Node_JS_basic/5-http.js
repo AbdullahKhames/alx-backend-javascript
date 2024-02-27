bd6820ed9f295873baaa68e9d6a3c162a8ebb28f
AbdullahKhames <abdullahkhames96@gmail.com>
2024-02-27 22:16:09 +0200

    readFileASync

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,17 +1,53 @@
 const http = require("http");
-const countStudents = require("./3-read_file_async");
 
 const path = process.argv[2];
-console.log(path);
 const app = http
   .createServer((req, res) => {
-    console.log(req.url);
     if (req.url == "/students") {
-      countStudents(path);
-      console.log("This is the list of our students");
+      res.write("This is the list of our students");
+      fs.readFile(path, "utf-8", (err, data) => {
+        if (err) {
+          throw new Error("Cannot load the database");
+        }
+
+        if (data) {
+          const students = data.split("\n");
+          students.shift();
+          let i = students.length - 1;
+          while (students[i] === "") {
+            students.pop();
+            i -= 1;
+          }
+          res.write(`Number of students: ${students.length}`);
+
+          const studentsPerCourse = {};
+          for (const s of students) {
+            const studentData = s.split(",");
+            const firstName = studentData[0];
+            const course = studentData[3];
+            if (course in studentsPerCourse) {
+              studentsPerCourse[course].push(firstName);
+            } else {
+              studentsPerCourse[course] = [firstName];
+            }
+          }
+
+          for (const course in studentsPerCourse) {
+            if (course) {
+              res.write(
+                `Number of students in ${course}: ${
+                  studentsPerCourse[course].length
+                }. List: ${studentsPerCourse[course].join(", ")}`
+              );
+            }
+          }
+        }
+      });
+      res.end();
+    } else {
+      res.write("Hello Holberton School!");
+      res.end();
     }
-    res.write("Hello Holberton School!");
-    res.end();
   })
   .listen(1245);
 
