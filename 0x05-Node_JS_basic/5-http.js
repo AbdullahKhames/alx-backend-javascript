1159d584f1fa961a203f4fce47baea3515541203
AbdullahKhames <abdullahkhames96@gmail.com>
2024-02-27 22:26:13 +0200

    readFileASync

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -2,52 +2,60 @@ const http = require("http");
 const fs = require("fs");
 
 const path = process.argv[2];
+function countStudents() {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, "utf-8", (err, data) => {
+      if (err) {
+        reject(new Error("Cannot load the database"));
+      }
+      if (data) {
+        const students = data.split("\n");
+        students.shift();
+        let i = students.length - 1;
+        while (students[i] === "") {
+          students.pop();
+          i -= 1;
+        }
+        const payload = [`Number of students: ${students.length}`];
+
+        const studentsPerCourse = {};
+        for (const s of students) {
+          const studentData = s.split(",");
+          const firstName = studentData[0];
+          const course = studentData[3];
+          if (course in studentsPerCourse) {
+            studentsPerCourse[course].push(firstName);
+          } else {
+            studentsPerCourse[course] = [firstName];
+          }
+        }
+
+        for (const course in studentsPerCourse) {
+          if (course) {
+            payload.push(
+              `Number of students in ${course}: ${
+                studentsPerCourse[course].length
+              }. List: ${studentsPerCourse[course].join(", ")}`
+            );
+          }
+        }
+        resolve(payload);
+      }
+    });
+  });
+}
 const app = http
   .createServer((req, res) => {
     if (req.url == "/students") {
       res.write("This is the list of our students");
-      fs.readFile(path, "utf-8", (err, data) => {
-        if (err) {
-          throw new Error("Cannot load the database");
-        }
-
-        if (data) {
-          const students = data.split("\n");
-          students.shift();
-          let i = students.length - 1;
-          while (students[i] === "") {
-            students.pop();
-            i -= 1;
-          }
-          res.write(`Number of students: ${students.length}`);
-
-          const studentsPerCourse = {};
-          for (const s of students) {
-            const studentData = s.split(",");
-            const firstName = studentData[0];
-            const course = studentData[3];
-            if (course in studentsPerCourse) {
-              studentsPerCourse[course].push(firstName);
-            } else {
-              studentsPerCourse[course] = [firstName];
-            }
-          }
-
-          for (const course in studentsPerCourse) {
-            if (course) {
-              res.write(
-                `Number of students in ${course}: ${
-                  studentsPerCourse[course].length
-                }. List: ${studentsPerCourse[course].join(", ")}`
-              );
-            }
-          }
-        }
-      });
+      countStudents()
+        .then((payload) => {
+          res.write(payload);
+        })
+        .catch((err) => console.log(err));
       res.end();
     } else {
-      res.write("Hello Holberton School!");
-      res.end();
+      res.end("Hello Holberton School!");
     }
   })
   .listen(1245);
