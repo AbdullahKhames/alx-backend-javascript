93ba86678ee4f617b876a3b01298c4147f45184e
AbdullahKhames <abdullahkhames96@gmail.com>
2024-02-27 17:49:50 +0200

    stdin demo

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-const readline = require("readline");
+const readline = require('readline');
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -7,18 +8,18 @@ const rl = readline.createInterface({
 
 if (process.stdin.isTTY) {
   rl.question(
-    "Welcome to Holberton School, what is your name?\n",
-    function (name) {
+    'Welcome to Holberton School, what is your name?\n',
+    (name) => {
       console.log(`Your name is: ${name}`);
-      console.log("This important software is now closing");
+      console.log('This important software is now closing');
       rl.close();
-    }
+    },
   );
 } else {
-  process.stdin.on("data", (name) => {
-    console.log("Welcome to Holberton School, what is your name?");
+  process.stdin.on('data', (name) => {
+    console.log('Welcome to Holberton School, what is your name?');
     process.stdout.write(`Your name is: ${name}`);
-    console.log("This important software is now closing");
+    console.log('This important software is now closing');
     process.exit();
   });
 }
