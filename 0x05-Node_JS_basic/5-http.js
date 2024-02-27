d3fcf4556b4963a04321f827797c1a8e00113995
AbdullahKhames <abdullahkhames96@gmail.com>
2024-02-27 22:09:03 +0200

    readFileASync

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,1 +1,17 @@
-#!/usr/bin/node
+const http = require("http");
+const countStudents = require("3-read_file_async.js");
+
+const path = process.argv[3];
+const app = http
+  .createServer((req, res) => {
+    console.log(req.url);
+    if (req.url == "/students") {
+      countStudents(path);
+      console.log("This is the list of our students");
+    }
+    res.write("Hello Holberton School!");
+    res.end();
+  })
+  .listen(1245);
+
+module.exports = app;
