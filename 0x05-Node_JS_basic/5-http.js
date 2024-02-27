358f85405c9ebe9e8002241b96f87b73f616d48e
AbdullahKhames <abdullahkhames96@gmail.com>
2024-02-27 22:27:43 +0200

    readFileASync

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,18 +1,18 @@
-const http = require("http");
-const fs = require("fs");
+const http = require('http');
+const fs = require('fs');
 
 const path = process.argv[2];
 function countStudents() {
   return new Promise((resolve, reject) => {
-    fs.readFile(path, "utf-8", (err, data) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
       if (err) {
-        reject(new Error("Cannot load the database"));
+        reject(new Error('Cannot load the database'));
       }
       if (data) {
-        const students = data.split("\n");
+        const students = data.split('\n');
         students.shift();
         let i = students.length - 1;
-        while (students[i] === "") {
+        while (students[i] === '') {
           students.pop();
           i -= 1;
         }
@@ -20,7 +20,7 @@ function countStudents() {
 
         const studentsPerCourse = {};
         for (const s of students) {
-          const studentData = s.split(",");
+          const studentData = s.split(',');
           const firstName = studentData[0];
           const course = studentData[3];
           if (course in studentsPerCourse) {
@@ -35,7 +35,7 @@ function countStudents() {
             payload.push(
               `Number of students in ${course}: ${
                 studentsPerCourse[course].length
-              }. List: ${studentsPerCourse[course].join(", ")}`
+              }. List: ${studentsPerCourse[course].join(', ')}`,
             );
           }
         }
@@ -46,16 +46,18 @@ function countStudents() {
 }
 const app = http
   .createServer((req, res) => {
-    if (req.url == "/students") {
-      res.write("This is the list of our students");
-      countStudents()
+    if (req.url === '/students') {
+      const message = 'This is the list of our students\n';
+      countStudents(process.argv[2])
         .then((payload) => {
-          res.write(payload);
+          res.end(message + payload.join('\n'));
         })
-        .catch((err) => console.log(err));
+        .catch((err) => {
+          res.end(message + err.message);
+        });
       res.end();
     } else {
-      res.end("Hello Holberton School!");
+      res.end('Hello Holberton School!');
     }
   })
   .listen(1245);
