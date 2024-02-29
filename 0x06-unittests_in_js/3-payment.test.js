62c3b124b36b8d845972055ce72e24f8b5dbabd5
AbdullahKhames <abdullahkhames96@gmail.com>
2024-02-29 23:41:47 +0200

    payment tests

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,1 +1,17 @@
-#!/usr/bin/node
+const { expect } = require("chai");
+const sinon = require("sinon");
+const sendPaymentRequestToApi = require("./3-payment");
+const Utils = require("./utils");
+
+describe("sendPaymentRequestToApi", () => {
+  it("should assert true to be true", () => {
+    expect(true).to.be.true;
+  });
+
+  it("should spy on a function using sinon", () => {
+    const spy = sinon.spy(Utils, "calculateNumber");
+    sendPaymentRequestToApi(100, 10);
+    expect(spy.calledOnce).to.be.true;
+    expect(spy.calledWith("SUM", 100, 10)).to.be.true;
+  });
+});
--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,1 +1,24 @@
 #!/usr/bin/node
+const Utils = {
+  calculateNumber: function calculateNumber(type, a, b) {
+    let result;
+    a = Math.round(a);
+    b = Math.round(b);
+    switch (type) {
+      case "SUM":
+        result = a + b;
+        break;
+      case "SUBTRACT":
+        result = a - b;
+        break;
+      case "DIVIDE":
+        if (b === 0) {
+          return "Error";
+        }
+        result = a / b;
+        break;
+    }
+    return result;
+  },
+};
+module.exports = Utils;
