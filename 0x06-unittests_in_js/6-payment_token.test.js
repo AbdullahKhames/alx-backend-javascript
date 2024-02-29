cbb314cedd11fc504abd55af643564c655eb625a
AbdullahKhames <abdullahkhames96@gmail.com>
2024-03-01 00:49:18 +0200

    testing promises

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,12 +1,24 @@
 #!/usr/bin/node
 const { expect } = require("chai");
-const sinon = require("sinon");
 const getPaymentTokenFromAPI = require("./6-payment_token");
-describe("getPaymentTokenFromAPI", function () {
-  it("test promise", function (done) {
-    getPaymentTokenFromAPI(true)
-    .then(data => {
-      expect(data).to.equal({ data: "Successful response from the API" });
+
+describe("getPaymentTokenFromAPI", () => {
+  it("should return a Promise", () => {
+    const result = getPaymentTokenFromAPI(true);
+    expect(result).to.be.an.instanceof(Promise);
+  });
+
+  it("should resolve with a data object when true", (done) => {
+    getPaymentTokenFromAPI(true).then((data) => {
+      expect(data).to.be.an("object");
+      expect(data).to.have.property("data");
+      done();
+    });
+  });
+
+  it("should resolve with the correct content", (done) => {
+    getPaymentTokenFromAPI(true).then((res) => {
+      expect(res).to.deep.equal({ data: "Successful response from the API" });
     });
     done();
   });
