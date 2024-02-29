39851dab915f1d2daf124dacf66d05b6fb61901a
AbdullahKhames <abdullahkhames96@gmail.com>
2024-03-01 00:34:00 +0200

    spy and stub

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,1 +1,25 @@
-#!/usr/bin/node
+const { expect } = require("chai");
+const sinon = require("sinon");
+const sendPaymentRequestToApi = require("./3-payment");
+const Utils = require("./utils");
+
+describe("sendPaymentRequestToApi", () => {
+  const spy = sinon.spy(console, "log");
+
+beforeEach(() => {
+  sinon.reset();
+});
+
+afterEach(() => {
+});
+  it("should spy on a function using sinon", () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(spy.calledOnce).to.be.true;
+    expect(spy.calledWith("The total is: 120")).to.be.true;
+  });
+  it("should spy on a function using sinon", () => {
+    sendPaymentRequestToApi(10, 10);
+    expect(spy.calledOnce).to.be.true;
+    expect(spy.calledWith("The total is: 20")).to.be.true;
+  });
+});
